34ae0ad624e3fa228a89b5c6aa1be68e3d266fc7
Philip Hazelden <philip.hazelden@gmail.com>
2009-05-13 17:06:26 +0100

    Fix bug 2, worsen bug 1.

--- src/net/philh/cloggle.clj
+++ src/net/philh/cloggle.clj
@@ -22,13 +22,14 @@
        ~@forms
        (glEnd)))
 
-(def gl-methods (seq (.getDeclaredMethods GL)))
-(def gl-fields
-     (map (fn [#^java.lang.reflect.Field m]
-	    (hash-map :name  (.getName m)
-		      :type  (.getType m)
-		      :value (.get m GL)))
-	  (seq (.getDeclaredFields GL))))
+(let [#^Class gl GL]
+  (def gl-methods (seq (.getDeclaredMethods gl)))
+  (def gl-fields
+       (map (fn [#^java.lang.reflect.Field m]
+	      (hash-map :name  (.getName m)
+			:type  (.getType m)
+			:value (.get m gl)))
+	    (seq (.getDeclaredFields gl)))))
 
 (defn def-ev
   "Like def, but evaluates its first argument. And (currently) doesn't add
@@ -38,11 +39,35 @@ metadata."
   ([#^Symbol name val]
      (intern *ns* name val)))
 
+;; getParameterTypes returns primitive types (int, float, etc.) and array types
+;; ([I, [F, etc.) when possible.
+;; The array types are easy to get, but I don't know of any way to get the
+;; primitive types without reflecting on a method which takes or returns them.
+(let [[iat fat dat] (map #(class (% 1 0)) [int-array float-array double-array])
+      [ipt fpt dpt] (map (fn [mname]
+			   (let [meth (first (filter #(= (.getName %) mname)
+						     (.getDeclaredMethods GL)))]
+			     (aget (.getParameterTypes meth) 0)))
+			 ["glVertex2i" "glVertex2f" "glVertex2d"])
+      tmap {ipt :int  fpt :float  dpt :double
+	    iat :ints fat :floats dat :doubles
+	    Integer :int Float :float Double :double}]
+  (defn ptypes->ktypes [ptypes]
+    (vec (map #(or (tmap %) %) (seq ptypes))))
+  (defn vals->ktypes [& vals]
+    (vec (map #(or (tmap (class %)) (class %)) vals))))
+
 (defn defn-from-method
   "Takes an instance method of GL and makes a function on opengl-context of it."
   [#^java.lang.reflect.Method meth]
-    (def-ev (symbol (.getName meth))
-      (fn [& args]
+    (let [name (.getName meth)
+	  multi
+	    (var-get (or (ns-resolve *ns* (symbol name))
+			 (def-ev (symbol name)
+			   (new clojure.lang.MultiFn name vals->ktypes
+				:default #'clojure.core/global-hierarchy))))
+	  params (.getParameterTypes meth)]
+      (defmethod multi (ptypes->ktypes params) [& args]
 	(.invoke meth opengl-context (to-array args)))))
 
 (doseq [i gl-fields]
--- src/net/philh/example/gears.clj
+++ src/net/philh/example/gears.clj
@@ -126,7 +126,7 @@
 	  (glVertex3d (* r1 (. Math cos angle))
 		      (* r1 (. Math sin angle))
 		      -width)
-	  (glNormal3d v1 (- u1) 0)
+	  (glNormal3d v1 (- u1) 0.0)
 	  (glVertex3d (* r2 (. Math cos (+ angle da)))
 		      (* r2 (. Math sin (+ angle da)))
 		      +width)
@@ -232,7 +232,7 @@
 				      (. drawable getChosenGLCapabilities))))
 		    (ctx gl
 		      (setSwapInterval 1)
-		      (.glLightfv opengl-context GL_LIGHT0 GL_POSITION pos 0)
+		      (glLightfv GL_LIGHT0 GL_POSITION pos 0)
 		      (glEnable GL_CULL_FACE)
 		      (glEnable GL_LIGHTING)
 		      (glEnable GL_LIGHT0)
@@ -240,22 +240,19 @@
 		    
 		      (dosync (ref-set gear1 (glGenLists 1)))
 		      (glNewList @gear1 GL_COMPILE)
-		      (.glMaterialfv opengl-context
-				     GL_FRONT GL_AMBIENT_AND_DIFFUSE red 0)
+		      (glMaterialfv GL_FRONT GL_AMBIENT_AND_DIFFUSE red 0)
 		      (gear 1.0 4.0 1.0 20 0.7)
 		      (glEndList)
 		    
 		      (dosync (ref-set gear2 (glGenLists 1)))
 		      (glNewList @gear2 GL_COMPILE)
-		      (.glMaterialfv opengl-context
-				     GL_FRONT GL_AMBIENT_AND_DIFFUSE green 0)
+		      (glMaterialfv GL_FRONT GL_AMBIENT_AND_DIFFUSE green 0)
 		      (gear 0.5 2.0 2.0 10 0.7)
 		      (glEndList)
 		    
 		      (dosync (ref-set gear3 (glGenLists 1)))
 		      (glNewList @gear3 GL_COMPILE)
-		      (.glMaterialfv opengl-context
-				     GL_FRONT GL_AMBIENT_AND_DIFFUSE blue 0)
+		      (glMaterialfv GL_FRONT GL_AMBIENT_AND_DIFFUSE blue 0)
 		      (gear 1.3 2.0 0.5 10 0.7)
 		      (glEndList)
 		    
